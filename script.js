264169cad22f5284a9f583e6d623af680d92f706
willalone <159647207+willalone@users.noreply.github.com>
2024-10-21 19:38:30 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const datepicker = flatpickr("#datepicker", {
-        mode: "multiple",  // или "range" для выбора диапазона
-        onChange: function(selectedDates) {
-            console.log(selectedDates); // Выводим выбранные даты в консоль
-        },
-        dateFormat: "Y-m-d", // Формат даты
-    });
-});
